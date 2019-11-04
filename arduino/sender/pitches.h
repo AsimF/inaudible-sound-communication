dced2206d4a4b9b2ce71415c40e6dda78919ee49
unknown <stateasimffa@gmail.com>
2019-11-04 17:51:40 +0900

    update

---
+++ arduino/sender/pitches.h
@@ -0,0 +1,12 @@
+//define the binary here for each letter/character
+
+int NOTE_A[8] {0,1,0,0,0,0,0,1};
+int NOTE_s[8] {0,1,1,1,0,0,1,1};
+int NOTE_i[8] {0,1,1,0,1,0,0,1};
+int NOTE_m[8] {0,1,1,0,1,1,0,1};
+int NOTE_SPACE[8] {0,0,1,0,0,0,0,0};
+int NOTE_F[8] {0,1,0,0,0,1,1,0};
+int NOTE_a[8] {0,1,1,0,0,0,0,1};
+int NOTE_u[8] {0,1,1,1,0,1,0,1};
+int NOTE_z[8] {0,1,1,1,1,0,1,0};
+int NOTE_NEWLINE[8] {0,0,0,0,1,1,0,1};
--- arduino/sender/sender.ino
+++ arduino/sender/sender.ino
@@ -1,11 +1,74 @@
+#include "pitches.h"
+
+//define the string to spell using their respective "binary" arrrays in pitches.h file
+int* spell_name[] = {
+  NOTE_A, NOTE_s, NOTE_i, NOTE_m, NOTE_SPACE, NOTE_F, NOTE_a, NOTE_u, NOTE_z, NOTE_i, NOTE_NEWLINE //"Asim\wFauzi\n"
+};
+
+//start the note on 19kHz frequency (all freqs later to be multiplied by 1000
+int start_note = 19;
+int current_note = start_note;
+
+//do nothing
 void setup() {
-  // put your setup code here, to run once:
-  //test
+  //nothing here
+}
+
+//loop through this thing until done...
+void loop() {
+
+  //loop through spell_name (has all my name's letters) then loop through each letter's binary digits and play the corresponding note (or frequency)
+  for(int i = 0; i < sizeof(spell_name); i++){
+    for(int j = 0; j < sizeof(spell_name[i]); j++){
+      tone(8, nextNote(current_note, spell_name[i][j]), 25);
+    }
+  }
   
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  //test
-  
+/*
+  19kHz -> 20kHz = 0 
+  21kHz -> 20kHz = 0
+  20kHz -> 21kHz = 0
+  19kHz -> 21kHz = 1
+  20kHz -> 19kHz = 1
+  21kHz -> 19kHz = 1
+
+  plays the next note based on the current note and what binary digit is needed
+*/
+int nextNote(int current, int want){
+  switch(want) {
+    case 0:
+       switch(current){
+        case 19:
+        current_note = 20;
+        return 20;
+        break;
+        case 20:
+        current_note = 21;
+        return 21;
+        break;
+        case 21:
+        current_note = 20;
+        return 20;
+        break;
+      }
+     break;
+    case 1:
+       switch(current){
+        case 19:
+        current_note = 21;
+        return 21;
+        break;
+        case 20:
+        current_note = 19;
+        return 19;
+        break;
+        case 21:
+        current_note = 19;
+        return 19;
+        break;
+      }
+     break;
+  }
 }
